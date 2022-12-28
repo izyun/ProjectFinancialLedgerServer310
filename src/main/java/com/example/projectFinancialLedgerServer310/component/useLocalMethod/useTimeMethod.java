a8a774c98690633fe574d1e1774ea444bea9a1c5
izyun <izyun@naver.com>
2022-12-28 17:24:28 +0900

    time method commit

--- src/main/java/com/example/projectFinancialLedgerServer310/component/useLocalMethod/useTimeMethod.java
+++ src/main/java/com/example/projectFinancialLedgerServer310/component/useLocalMethod/useTimeMethod.java
@@ -2,6 +2,7 @@ package com.example.projectFinancialLedgerServer310.component.useLocalMethod;
 
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -11,9 +12,29 @@ public class useTimeMethod {
 
     public static String timeData() {
         LocalDateTime dateTime = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("YYYY/MM/dd HH:mm:ss");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("YYYY-MM-dd HH:mm:ss");
         String logDate = dateTime.format(formatter);
         return logDate;
+    }
+    public static String currentYear() {
+        LocalDate date = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("YYYY");
+        String logYear = date.format(formatter);
+        return logYear;
+    }
+
+    public static String currentMonth() {
+        LocalDate date = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM");
+        String logMonth = date.format(formatter);
+        return logMonth;
+    }
+
+    public static String currentDay() {
+        LocalDate date = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd");
+        String logDay = date.format(formatter);
+        return logDay;
     }
 
 }
